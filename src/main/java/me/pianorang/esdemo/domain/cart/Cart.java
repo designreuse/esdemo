f4eac548d2bbfe842430a75503e6e852134e8d93
Brahim MISSAOUI <brahim.missaoui@design-reuse.com>
2024-10-28 13:40:03 +0100

    first commit

---
+++ src/main/java/me/pianorang/esdemo/domain/cart/Cart.java
@@ -0,0 +1,10 @@
+package me.pianorang.esdemo.domain.cart;
+
+public class Cart {
+    String title;
+    String content;
+    String author;
+    String url;
+    String publishedAt;
+
+}
---
+++ src/main/java/me/pianorang/esdemo/domain/stock/Stock.java
@@ -0,0 +1,10 @@
+package me.pianorang.esdemo.domain.stock;
+
+public class Stock {
+    String title;
+    String content;
+    String author;
+    String url;
+    String publishedAt;
+
+}
